b5a5e2a2747421039aced0a3ac137e0e8b011018
nisda <na.ishida.0115@gmail.com>
2026-05-05 16:00:58 +0900

    job/table add explode and fix

--- src/SmallETL/job/built_in/table.py
+++ src/SmallETL/job/built_in/table.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Any, Literal, Type
 
-from ...libs.data_table import DataTable
+from .core.data_table import DataTable
 
 
 
@@ -8,15 +8,22 @@ def filter(data:Any, condition:Dict[str, Any]|List[Dict[str, Any]]):
     """フィルタリング（条件抽出）"""
     dt_in = DataTable(data=data)
     dt_out = dt_in.filter(condition=condition)
-    return dt_out.rows()
+    return dt_out.rows(type='dict')
 
+
+def filter_multiple(data:Any, conditions:List[Dict[str, Any]|List[Dict[str, Any]]]):
+    """フィルタリング（条件抽出）複数"""
+    dt = DataTable(data=data)
+    for condition in conditions:
+        dt = dt.filter(condition=condition)
+    return dt.rows(type='dict')
 
 
 def grouping(data:Any, group_by:List[str]=[], aggregation:Dict[str, str]={}):
     """グルーピング"""
     dt_in = DataTable(data=data)
     dt_out = dt_in.group_by(group_by=group_by, aggregation=aggregation)
-    return dt_out.rows()
+    return dt_out.rows(type='dict')
 
 
 
@@ -35,7 +42,7 @@ def join(
     dt_left = DataTable(data=params.pop("data_left"))
     dt_right = DataTable(data=params.pop("data_right"))
     dt_out = dt_left.join(table=dt_right, **params)
-    return dt_out.rows()
+    return dt_out.rows(type='dict')
 
 
 
@@ -43,7 +50,7 @@ def sort(data, sort_by:List[str]):
     """ソート"""
     dt_in = DataTable(data=data)
     dt_out = dt_in.sort(sort_by=sort_by)
-    return dt_out.rows()
+    return dt_out.rows(type='dict')
 
 
 
@@ -55,39 +62,56 @@ def convert(
         is_null :Any = None,
         null_if :Any = None,
         errors :Literal['raise', 'coerce', 'ignore'] = 'raise',
-        error_data: List[Any] = None,
-        repeat: List[Dict] = None,  # 繰り返し処理用のパラメータ。
     ):
     """データ変換"""
 
     params_:Dict[str, Any] = locals()
     data = params_.pop("data")
-    repeat = params_.pop("repeat")
 
-    # パラメータをリスト形式に揃える。
-    if not repeat:
-        repeat = [params_]
 
     # テーブル生成
     dt_in = DataTable(data=data)
 
     # 繰り返し処理
-    error_list = []
-    for param in repeat:
-        key = param["column"]
-        error_data = []
-        # 変換実行
-        dt_in[key] = dt_in.convert(**param, error_data=error_data)
-        error_list.append(error_data)
+    error_data = []
 
-    dt_out = dt_in.convert(**params_)
-    return dt_out.rows()
+    # 変換実行
+    key = params_["column"]
+    dt_in[key] = dt_in.convert(**params_, error_data=error_data)
 
+    return {
+        "output" : dt_in.rows(type='dict'),
+        "errors" : error_data,
+    }
+
+def convert_multiple(data, params:List[Dict]):
+    """データ変換（複数回一括）"""
+
+    errors: List = []
+    for param in params:
+        ret = convert(data, **param)
+        data = ret["output"]
+        errors.append(ret["errors"])
+
+    return {
+        "output" : data,
+        "errors" : errors,
+    }
 
 def rename(data, columns:Dict|List):
     """カラム名リネーム"""
     dt_in = DataTable(data=data)
     dt_out = dt_in.rename(columns=columns)
-    return dt_out.rows()
+    return dt_out.rows(type='dict')
+
+
+def explode(data, keys:List[str], sep:str='_'):
+    if isinstance(keys, str):
+        keys = [keys]
+
+    dt = DataTable(data=data)
+    for key in keys:
+        dt = dt.explode(key=key, sep=sep)
+    return dt.rows(type='dict')
 
 
