1cdd9e5b58e3f5951af9bf6314dad14b05e150ca
nisda <na.ishida.0115@gmail.com>
2026-05-11 22:34:15 +0900

    fix: http job

--- src/SmallETL/job/built_in/http.py
+++ src/SmallETL/job/built_in/http.py
@@ -2,14 +2,8 @@ from typing import List, Dict, Final
 from datetime import datetime, date
 from .core.http_requests.http_requests_plus import RequestsPlus
 
-__WEEKDAYS_JP:Final[str] = ['月', '火', '水', '木', '金', '土', '日']
-__WEEKDAYS_EN:Final[str] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
 def get_text(url:str):
 
-    #-----------------------------------------
-    #   内閣府の公開するリストから取得
-    #-----------------------------------------
     params = {
         "base_url"       : url,
         "retry_count"    : 4,
